b65cc5caff4a4f4e9f748ad7b7ca687fdc0163c6
Reini Urban <reini@spexai.com>
2025-10-18 08:30:12 +1100

    Release 1.29

--- lib/B/Keywords.pm
+++ lib/B/Keywords.pm
@@ -14,7 +14,7 @@ use vars qw( @EXPORT_OK %EXPORT_TAGS );
 %EXPORT_TAGS = ( 'all' => \@EXPORT_OK );
 
 use vars '$VERSION';
-$VERSION = '1.28';
+$VERSION = '1.29';
 my $CPERL = $^V =~ /c$/ ? 1 : 0;
 
 use vars '@Scalars';
